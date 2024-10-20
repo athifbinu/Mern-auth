11eac84d8d5f0bd5406f3ed9c65722d71ebda87a
Athif Binu <binuathif@gmail.com>
2024-10-20 15:44:03 +0530

    add signup route

--- server/controllers/auh.control.js
+++ server/controllers/auh.control.js
@@ -1,3 +1,21 @@
-export const sinup = (req, res) => {
-  console.log(req.body);
+import User from "../models/user_model.js";
+import bycriptjs from "bcryptjs";
+
+export const sinup = async (req, res) => {
+  const { username, email, password } = req.body;
+  //this line useed to bycript the password
+  const hashedPassword = bycriptjs.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json;
+    error.message;
+  }
 };
